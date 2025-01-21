129d79b22cad3199424f9ac0ed0f9c15797ac91e
Belgelerim <Belgelerim@DESKTOP-T8DBDKP>
2025-01-21 21:35:13 +0300

    update

--- To-Do List Manager/Program.cs
+++ To-Do List Manager/Program.cs
@@ -13,152 +13,154 @@ namespace To_Do_List_Manager
     static class FileLocations
     {
         public static string saveTask = "TList.xml";
-    }
-    internal class Program
-    {
-        static string AskAndReceive(string question)
+        internal class Program
         {
-            Console.Write(question);
-            return Console.ReadLine();
-        }
-        static void Main(string[] args)
-        {
-            bool isdone = false;
-            Console.WriteLine("Welcome to the program");
-            while (!isdone)
+            static string AskAndReceive(string question)
             {
-                File.Open(FileLocations.saveTask, FileMode.OpenOrCreate).Close();
-                Console.WriteLine("What would you like to do");
-                Console.WriteLine("1: Create new task");
-                Console.WriteLine("2: Check the tasks");
-                Console.WriteLine("3: Delete Task");
-                Console.WriteLine("4: Exit");
-                string userChoice = AskAndReceive("Your choice: ");
-                while (userChoice != "1" && userChoice != "2" && userChoice != "3" && userChoice != "4")
+                Console.Write(question);
+                return Console.ReadLine();
+            }
+            static void Main(string[] args)
+            {
+                bool isdone = false;
+                Console.WriteLine("Welcome to the program");
+                while (!isdone)
                 {
-                    Console.Write("Choose 1, 2, 3 or 4: ");
-                    userChoice = Console.ReadLine();
+                    File.Open(FileLocations.saveTask, FileMode.OpenOrCreate).Close();
+                    Console.WriteLine();
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("What would you like to do");
+                    Console.WriteLine("1: Create new task");
+                    Console.WriteLine("2: Check the tasks");
+                    Console.WriteLine("3: Delete Task");
+                    Console.WriteLine("4: Change the status of a task");
+                    Console.WriteLine("5: Exit");
+                    string userChoice = AskAndReceive("Your choice: ");
+                    while (userChoice != "1" && userChoice != "2" && userChoice != "3" && userChoice != "4" && userChoice != "5")
+                    {
+                        userChoice = AskAndReceive("Choose between 1, 2, 3, 4 or 5: ");
+                    }
+                    switch (userChoice)
+                    {
+                        case "1":
+                            CreateNewTask(args);
+                            break;
+                        case "2":
+                            CheckTasks();
+                            break;
+                        case "3":
+                            int deleteTask = Convert.ToInt32(AskAndReceive("Which task will you delete(Use numbers): "));
+                            DeleteATask(FileLocations.saveTask, deleteTask);
+                            break;
+                        case "4":
+                            int changeTask = Convert.ToInt32(AskAndReceive("Which task will you change(Use numbers): "));
+                            ChangeStatus(FileLocations.saveTask, changeTask);
+                            break;
+                        case "5":
+                            isdone = true;
+                            Console.WriteLine("Goodbye!");
+                            break;
+                        default:
+                            continue;
+                    }
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine();
                 }
+            }
+            static void CreateNewTask(string[] args)
+            {
+                try
+                {
+                    string name = AskAndReceive("Name: ");
+                    while (name == "")
+                    {
+                        name = AskAndReceive("Name can't be empty: ");
+                    }
+                    string description = AskAndReceive("Description: ");
+                    while (description == "")
+                    {
+                        description = AskAndReceive("Description can't be empty: ");
+                    }
+                    string iss = AskAndReceive("Is it complete(T for True/F for False): ").Replace(" ", "").ToUpper();
+                    bool iscomplete = false;
+                    while (iss != "T" && iss != "F")
+                    {
+                        switch (iss)
+                        {
+                            case "T":
+                                iscomplete = true;
+                                break;
+                            case "F":
+                                iscomplete = false;
+                                break;
+                            default:
+                                iss = AskAndReceive("Use 'T' or 'F': ").Replace(" ", "").ToUpper();
+                                continue;
+                        }
+                    }
 
-                if (userChoice == "1")
-                {
-                    CreateNewTask(args);
+
+                    TaskList.Add(new Task(name, description, iscomplete));
+                    TaskList.update(FileLocations.saveTask);
+                    Console.WriteLine("Succesfully done!");
                 }
-                if (userChoice == "2")
+                catch (Exception ex)
                 {
-                    CheckTasks();
+                    Console.WriteLine(ex.Message);
                 }
-                if (userChoice == "3")
+            }
+            static void CheckTasks()
+            {
+                try
                 {
-                    Console.Write("Which task will you delete(Use numbers): ");
-                    DeleteATask(FileLocations.saveTask, Convert.ToInt32(Console.ReadLine()));
+                    Console.WriteLine("**********************************************************************");
+                    Console.WriteLine("Tasks:");
+                    if (File.ReadAllText(FileLocations.saveTask) == "")
+                    {
+                        Console.WriteLine("There is no task.");
+                    }
+                    else
+                        Console.WriteLine(File.ReadAllText(FileLocations.saveTask));
+                    Console.WriteLine("**********************************************************************");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            static void DeleteATask(string fileName, int line_to_edit)
+            {
+                try
+                {
+                    List<string> arrLine = File.ReadAllLines(fileName).ToList();
+                    arrLine.RemoveAt(line_to_edit - 1);
+                    File.WriteAllLines(fileName, arrLine);
                     Console.WriteLine("Task deleted.");
                 }
-                if (userChoice == "4")
+                catch (Exception ex)
                 {
-                    isdone = true;
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            static void ChangeStatus(string fileName, int line_to_edit)
+            {
+                try
+                {
+                    string[] lines = File.ReadLines(fileName).ToArray();
+                    if (lines[line_to_edit - 1].Contains("isn't completed"))
+                        lines[line_to_edit - 1] = lines[line_to_edit - 1].Replace("isn't completed", "is completed");
+                    else
+                        lines[line_to_edit - 1] = lines[line_to_edit - 1].Replace("is completed", "isn't completed");
+                    File.WriteAllLines(fileName, lines);
+                    Console.WriteLine("Status succesfully changed.");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
-        static void CheckTasks()
-        {
-            try
-            {
-                Console.WriteLine(File.ReadAllText(FileLocations.saveTask));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void DeleteATask(string fileName, int line_to_edit)
-        {
-            try
-            {
-                List<string> arrLine = File.ReadAllLines(fileName).ToList();
-                arrLine.RemoveAt(line_to_edit - 1);
-                File.WriteAllLines(fileName, arrLine);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void CreateNewTask(string[] args)
-        {
-            try
-            {
-                string name = AskAndReceive("Name: ");
-                string description = AskAndReceive("Description: ");
-                string iss = AskAndReceive("Is it complete(T/F): ").Replace(" ", "");
 
-                bool iscomplete;
-
-                while (iss != "T" && iss != "F")
-                {
-                    iss = AskAndReceive("Use 'T' or 'F': ").Replace(" ", "");
-                }
-
-                if (iss == "T")
-                {
-                    iscomplete = true;
-                }
-                else
-                {
-                    iscomplete = false;
-                }
-
-                TList.Add(new Task(name, description, iscomplete));
-                TList.update(FileLocations.saveTask);
-                Console.WriteLine("Succesfully done!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            }
-    }
-    class Task
-    {
-        public string Name;
-        public string Description;
-        public bool iscomplete;
-        public Task(string Name, string Description, bool iscomplete)
-        {
-            this.Name = Name;
-            this.Description = Description;
-            this.iscomplete = iscomplete;
-        }
-        public override string ToString()
-        {
-            string ss;
-            if (!iscomplete)
-            {
-                ss = "This task isn't completed.";
-            }
-            else
-            {
-                ss = "This task is completed.";
-            }
-            return Convert.ToString(Name +"-"+ Description +"-" + ss);
-        }
-    }
-    class TList
-    {
-        public static Task taskT;
-        public static void update(string saveTask)
-        {
-            List<string> lines = new List<string>();
-            lines = File.ReadLines(saveTask).ToList();
-            File.AppendAllLines(saveTask, new[] { taskT.ToString() });
-        }
-        public static void Tell()
-        {
-            Console.WriteLine(taskT);
-        }
-        public static void Add(Task task)
-        {
-            taskT = task;
-        }
     }
 }
---
+++ To-Do List Manager/TaskClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Do_List_Manager
+{
+    sealed public class Task
+    {
+        public string Name;
+        public string Description;
+        public bool iscomplete;
+        public Task(string Name, string Description, bool iscomplete)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.iscomplete = iscomplete;
+        }
+        public override string ToString()
+        {
+            string ss;
+            if (!iscomplete)
+            {
+                ss = "This task isn't completed.";
+            }
+            else
+            {
+                ss = "This task is completed.";
+            }
+            return Convert.ToString($"{Name} - {Description} - {ss}");
+        }
+    }
+    abstract public class TaskList
+    {
+        public static Task taskT;
+        public static void update(string saveTask)
+        {
+            List<string> lines = new List<string>();
+            lines = File.ReadLines(saveTask).ToList();
+            File.AppendAllLines(saveTask, new[] { taskT.ToString() });
+        }
+        public static void Tell()
+        {
+            Console.WriteLine(taskT);
+        }
+        public static void Add(Task task)
+        {
+            taskT = task;
+        }
+        public static void changeStatus(string saveTask, int order)
+        {
+        }
+    }
+}
